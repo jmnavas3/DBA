6a40f5d32091df27b3999ab8b24896297ce205f5
Galvez134 <manu52gg@correo.ugr.es>
2023-11-02 10:31:38 +0100

    feature/Initial-P2

---
+++ src/pr2/Enviroment.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pr2;
+
+/**
+ *
+ * @author galvez
+ */
+public class Enviroment {
+    private MapDto map;
+    private int goalPositionX, goalPositionY;
+    private int agentPositionX, agentPositionY;
+    private int[] sensors;
+    
+    public Enviroment(MapDto map){
+        
+    }
+    
+    // Update agent position 
+    public void doMoveAction(String action){
+        
+    }
+    
+    // Update sensors
+    public void see(){
+        
+    }
+    
+    public void setAgentPosition(int x, int y){
+        
+    }
+    
+    public void setGoalPosition(int x, int y){
+        
+    }
+    
+    public void setMap(MapDto map){
+        
+    }
+}
+
