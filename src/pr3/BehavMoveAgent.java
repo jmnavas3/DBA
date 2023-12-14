b3291c83c1c7dabd63712b8f1372740754e3e42c
jmnavas3 <joselillo.sk8@gmail.com>
2023-12-14 15:48:30 +0100

    movement behav refactored

--- src/pr3/BehavMoveAgent.java
+++ src/pr3/BehavMoveAgent.java
@@ -1,15 +1,7 @@
 package pr3;
 
-import jade.core.behaviours.Behaviour;
-import jade.core.behaviours.DataStore;
 
-/**
- *
- * @author galvez
- */
-public class BehavMoveAgent extends Behaviour{
-    DataStore ds;
-    
+public class BehavMoveAgent extends BehavGoal{
     @Override
     public void action() {
         ds = this.getDataStore();
@@ -21,28 +13,5 @@ public class BehavMoveAgent extends Behaviour{
         
         ds.put("enviroment", env);
         this.setDataStore(ds);
-    }
-    
-   @Override
-    public boolean done() {
-        ds = this.getDataStore();
-        Enviroment env = (Enviroment) ds.get("enviroment");
-        boolean buscandoRenos = (boolean) ds.get("buscareno");
-        boolean buscandoSanta = (boolean) ds.get("buscasanta");
-        boolean goalReached = env.checkGoal();
-        boolean lastGoalReached = false;
-        
-        if (buscandoRenos && goalReached){
-            System.out.println("Reno encontrado!");
-            ds.put("buscareno", false);
-        }
-        
-        if (buscandoSanta && goalReached) {
-            lastGoalReached = true;
-            System.out.println("Santa encontrado!");
-            ds.put("buscasanta", false);
-        }
-        
-        return lastGoalReached;
     }
 }
--- src/pr3/BehavSee.java
+++ src/pr3/BehavSee.java
@@ -1,28 +1,12 @@
 package pr3;
 
-import jade.core.behaviours.Behaviour;
-import jade.core.behaviours.DataStore;
-
-
-/**
- *
- * @author galvez
- */
-public class BehavSee extends Behaviour {
-    
+public class BehavSee extends BehavGoal {
     @Override
     public void action() {
-        DataStore ds = this.getDataStore();
+        ds = this.getDataStore();
         Enviroment env = (Enviroment) ds.get("enviroment");
         env.see();
         ds.put("enviroment", env);
         this.setDataStore(ds);
-    }
-    
-    @Override
-    public boolean done() {
-        DataStore ds = this.getDataStore();
-        Enviroment env = (Enviroment) ds.get("enviroment");
-        return env.checkGoal();
     }
 }
