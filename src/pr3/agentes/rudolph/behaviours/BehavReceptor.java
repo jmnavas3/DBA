d701dcf894b93e271bef3a3039da8731290de4a9
jmnavas3 <joselillo.sk8@gmail.com>
2023-12-20 16:11:01 +0100

    Rudolph Behaviour clean

--- src/pr3/agentes/rudolph/behaviours/BehavReceptor.java
+++ src/pr3/agentes/rudolph/behaviours/BehavReceptor.java
@@ -5,28 +5,27 @@ import jade.core.behaviours.DataStore;
 import jade.lang.acl.ACLMessage;
 import java.util.ArrayList;
 
-
 public class BehavReceptor extends Behaviour {
 
-    private static enum Paso {RECIBIR_CODIGO, DAR_COORD};
-    private Paso step = Paso.RECIBIR_CODIGO;
+    DataStore ds;
     ACLMessage respuesta;
     ArrayList<Coords> renos;
-    DataStore ds;
+
+    private static enum Paso {VALIDAR_CODIGO, DAR_COORD};
+    private Paso step = Paso.VALIDAR_CODIGO;
 
     @Override
     public void action() {
         ds = this.getDataStore();
         renos = (ArrayList<Coords>) ds.get("renos");
         String codigo = (String) ds.get("codigo");
-        
+
         switch (step) {
-            case RECIBIR_CODIGO -> {
+            case VALIDAR_CODIGO -> {
                 ACLMessage check_code = myAgent.blockingReceive();
                 dialogo("dejame comprobar el codigo");
-
-                if (check_code.getPerformative() == ACLMessage.REQUEST &&
-                        check_code.getConversationId().equals(codigo)) {
+                if (check_code.getPerformative() == ACLMessage.REQUEST
+                        && check_code.getConversationId().equals(codigo)) {
                     dialogo("el codigo es correcto!");
                     responder(check_code, ACLMessage.CONFIRM);
                     step = Paso.DAR_COORD;
@@ -35,42 +34,31 @@ public class BehavReceptor extends Behaviour {
                     messageError("CODIGO INCORRECTO!");
                 }
             }
+            
             case DAR_COORD -> {
-                // esperamos a que nos pida unas coordenadas
                 ACLMessage request_coords = myAgent.blockingReceive();
                 dialogo("voy a ver mi radar...");
-
-                if (request_coords.getPerformative() == ACLMessage.REQUEST) {
-                    // comprobamos si quedan renos y respondemos
-                    dar_coord(request_coords);
+                if (request_coords.getPerformative() == ACLMessage.REQUEST
+                        && request_coords.getConversationId().equals(codigo)) {
+                    responder(request_coords, ACLMessage.INFORM);
                 } else {
                     messageError("Error en paso " + step);
                 }
             }
         }
     }
-    
 
-    public void messageError(String info) {
-        dialogo(info);
-         myAgent.doDelete();
-    }
-    
-    
     public void dialogo(String frase) {
         System.out.println(myAgent.getAID().getLocalName() + ": " + frase);
     }
 
-    
     public void responder(ACLMessage mensaje, int perf) {
         respuesta = mensaje.createReply(perf);
+        if (step == Paso.DAR_COORD) dar_coord();
         myAgent.send(respuesta);
     }
 
-    
-    public void dar_coord(ACLMessage mensaje) {
-        respuesta = mensaje.createReply(ACLMessage.INFORM);
-        
+    public void dar_coord() {
         if (!renos.isEmpty()) {
             dialogo("Si! quedan " + renos.size() + ". Aqui tienes su ubicacion");
             respuesta.setContent(renos.remove(0).toString());
@@ -80,11 +68,13 @@ public class BehavReceptor extends Behaviour {
             respuesta.setContent("buscasanta");
             ds.put("terminar", true);
         }
-        
         this.setDataStore(ds);
-        myAgent.send(respuesta);
     }
-
+    
+    public void messageError(String info) {
+        dialogo(info);
+        myAgent.doDelete();
+    }
 
     @Override
     public boolean done() {
